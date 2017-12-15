213cdbfc1caafb4c3458243e6a52ed74529feafd
Vinson Zhang <vinson.zhang@gmail.com>
2017-12-15 17:25:30 -0500

    Update GetFilePathFromClasspath.scala

--- GetFilePathFromClasspath.scala
+++ GetFilePathFromClasspath.scala
@@ -1,6 +1,6 @@
- def getFileFromClasspath(fileAbsPath: String): Option[String] = {
-    val sourceCSVFileResource = getClass.getResource(fileAbsPath)
-    Option(sourceCSVFileResource) match {
+ def getFileFromClasspath(classPath: String): Option[String] = {
+    val resource = getClass.getResource(classPath)
+    Option(resource) match {
       case None => None
       case Some(x) => Some(x.getPath.toString())
     }
