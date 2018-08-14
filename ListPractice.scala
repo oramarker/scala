cb2a13ca88c0cf82282c07d27aa1e26a9cef1740
Vinson Zhang <vinson.zhang@gmail.com>
2018-08-14 08:48:04 -0400

    Update ListPractice.scala

--- ListPractice.scala
+++ ListPractice.scala
@@ -98,3 +98,54 @@ class ListNote {
     case h:: tail if h > n => getCoinSet(tail,n)
     case h::tail =>  getCoinSet(xs,n-h).map(x=>h::x) ::: getCoinSet(tail,n) ::: (if(n ==h) List(List(h)) else Nil)
   }
+  
+  
+  /** Checks brackets in string are balanced or not */
+  def balanced(s: String): Boolean = {
+
+    @tailrec
+    def bal(xs: List[Char], stack: List[Char]): Boolean = xs match {
+      case Nil => if (stack.isEmpty) true else false
+      case h :: tail if (h == '(') => bal(tail, h :: stack)
+      case h :: tail if (h == ')') => if (stack.isEmpty) false else bal(tail, stack.tail)
+      case h :: tail => bal(tail, stack)
+    }
+
+    bal(s.toList, List[Char]())
+  }
+  
+  /* Check there is pair of ints that add up to the target */
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var map = scala.collection.mutable.Map[Int, Int]()
+    val result = new Array[Int](2)
+    nums.indices.foreach(i => {
+      val value = map.get(nums(i))
+      if (value.isDefined) {
+        result(0) = value.get
+        result(1) = i
+        return result
+      }
+      map(target - nums(i)) = i
+    })
+
+    result
+
+  }
+  
+  /* Add the the numbers in the two list */
+  def addTwoNum(num1: List[Int], num2: List[Int]): List[Int] = {
+    @tailrec
+    def myAdd(num1: List[Int], num2: List[Int], c: Int, res: List[Int]): List[Int] = (num1, num2) match {
+      case (Nil, Nil) => if (c == 0) res else res ::: List(c)
+      case (Nil, n2) => myAdd(List(c), n2, 0, res)
+      case (n1, Nil) => myAdd(n1, List(c), 0, res)
+      case (h1 :: tail1, h2 :: tail2) => {
+        val sum = h1 + h2 + c
+        myAdd(tail1, tail2, sum / 10, res ::: List(sum % 10))
+      }
+    }
+
+    myAdd(num1, num2, 0, List[Int]())
+
+  }
+
